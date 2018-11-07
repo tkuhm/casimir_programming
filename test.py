1f9d889c17536268a51b66d18aa25670f0904b53
Tanja <t.i.kuhm@tudelft.nl>
2018-11-07 13:23:29 +0100

    my second commit

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print('hello world')
+print("hello world!")
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+p = 5
+print("The radius of the circle is", p)
+
+def circumference(r):
+    """This function calculates the circumference of a circle"""
+    return 2*np.pi*r
+
+print("the circumference of the circle is", circumference(p))
+
+def surface_area(r):
+    """ this is a test to see how to combine two versions """
+    return np.pi*r**2
+
+print("The surface area of the circle is", surface_area(p))
+
+plt.Circle((0, 0), 1, color='blue')
