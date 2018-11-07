fc4f632595cf186255c3b482f8003e1196a2d443
ElsSweep <e.sweep@tudelft.nl>
2018-11-07 16:35:33 +0100

    now only imports part of excel file we want to use

--- project.py
+++ project.py
@@ -1,26 +1,18 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_excel('Ilses_biotek_exp.xlsx', skiprows=30)
 
-
-
-df_time = df['Time']
-df_time_data = df_time[1:480]
-print(df_time_data)
-
+#import excel file (file name, number of first rows to skip, number of rows to use, which cols to use)
+df = pd.read_excel('Ilses_biotek_exp.xlsx', skiprows=30, nrows=481, usecols="B:CU")
 
 #print the column names
 print(df.columns)
 
-# #get the values for a given column
+#get the values for a given column
 values = df['Time'].values
-
 print(values)
 
-# #get a data frame with selected columns
-# FORMAT = ['Time', 'A1', 'A2']
-# df_selected = df[FORMAT]
-
-# print(df_selected)
-# # print(df_data['Time', 'A1'])
+#get a data frame with selected columns
+FORMAT = ['Time', 'A1', 'A2']
+df_selected = df[FORMAT]
+print(df_selected)
