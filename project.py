a3a524dba53364c4ed4c481fd17fccdbde3e396c
ElsSweep <e.sweep@tudelft.nl>
2018-11-08 12:40:15 +0100

    update the project, project.py is going to be the main file.

--- project.py
+++ project.py
@@ -1,19 +1,34 @@
 import numpy as np
 import pandas as pd
 from datetime import timedelta
+import matplotlib
+matplotlib.use("Agg") # this you need for new version of matplotlib to work
+import matplotlib.pyplot as plt
 
+
+#nr. of timesteps
+nrows = 481
+
+#to fix timesteps
+time_step_seconds = 360
+time_start_seconds = 310
 
 #import excel file (file name, number of first rows to skip, number of rows to use, which cols to use)
-df = pd.read_excel('Ilses_biotek_exp.xlsx', skiprows=30, nrows=481, usecols="B:CU")
+df = pd.read_excel('Ilses_biotek_exp.xlsx', skiprows=30, nrows=nrows, usecols="B:CU")
 
-#print the column names
-print(df.columns)
 
-#get the values for a given column
-values = df['Time'].values
-print(values)
+#new timesteps
+seconds_since_start = np.arange(nrows)*time_step_seconds+time_start_seconds
+time_since_start = pd.to_timedelta(seconds_since_start, unit='s')
 
-#get a data frame with selected columns
-FORMAT = ['Time', 'A1', 'A2']
-df_selected = df[FORMAT]
-print(df_selected)
+df['Time'] = time_since_start
+
+
+#sets time as index
+df.set_index('Time', inplace=True)
+
+#plots a graph
+df.C11.plot()
+plt.xlabel(r'timestep', size=15)
+plt.ylabel(r'OD600', size=15)
+plt.title('growth curve of C11', size=20)
